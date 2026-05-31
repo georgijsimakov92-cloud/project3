a4a4d79fb422fa219f1f00d86c6891178fc0fa97
georgijsimakov92-cloud <georgijsimakov92@gmail.com>
2026-05-31 17:29:14 +0500

    Update BulletObject.java

--- core/src/ru/samsung/gamestudio/objects/BulletObject.java
+++ core/src/ru/samsung/gamestudio/objects/BulletObject.java
@@ -1,8 +1,8 @@
-package ru.samsung.gamestudio.objects;
+package simakov.licey97.objects;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
-import ru.samsung.gamestudio.GameSettings;
+import simakov.licey97.GameSettings;
 
 public class BulletObject extends GameObject {
 
