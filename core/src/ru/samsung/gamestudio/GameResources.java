2ff30f8bbd239ba27059df13cbd054012149d00b
georgijsimakov92-cloud <georgijsimakov92@gmail.com>
2026-05-31 17:38:06 +0500

    Update GameResources.java

--- core/src/ru/samsung/gamestudio/GameResources.java
+++ core/src/ru/samsung/gamestudio/GameResources.java
@@ -1,8 +1,7 @@
-package ru.samsung.gamestudio;
+package simakov.licey97;
 
 public class GameResources {
 
-    // Images for textures
 
     public static final String BACKGROUND_IMG_PATH = "textures/background.png";
 
@@ -20,13 +19,11 @@ public class GameResources {
     public static final String SHIP_IMG_PATH = "textures/ship.png";
     public static final String TRASH_IMG_PATH = "textures/trash.png";
 
-    // Sounds and music for audio
 
     public static final String BACKGROUND_MUSIC_PATH = "sounds/background_music.mp3";
     public static final String DESTROY_SOUND_PATH = "sounds/destroy.mp3";
     public static final String SHOOT_SOUND_PATH = "sounds/shoot.mp3";
 
-    // Fonts for text
 
     public static final String FONT_PATH = "fonts/Montserrat-Bold.ttf";
 
